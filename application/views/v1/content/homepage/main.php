a4d2eb1a2a2222d9c47660fafd2913c8530ec0dc
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-15 11:53:55 +0100

    added simplegrid framework; started styling hompeage content section

--- application/views/v1/content/homepage/main.php
+++ application/views/v1/content/homepage/main.php
@@ -1,3 +1,74 @@
 <?php $this->load->view('v1/content/homepage/sub_header'); ?>
 
-test
+<div class="content-homepage">
+  <div class="content-row grid grid-pad">
+    <div class="col-1-3">
+      <h2 class="category-heading">Updates</h2>
+    </div>
+    <div class="col-2-3">
+      <h2 class="category-heading">Guidance &amp; Support</h2>
+    </div>
+
+    <div>
+      <article class="news-item col-1-3">
+        <img src="" />
+        <span class="timestamp">29 August 2014</span>
+        <h3>UK terrorist threat level raised</h3>
+        <p class="content">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh. Nunc varius facilisis  eros. Sed erat. In in velit quis
+        </p>
+      </article>
+      <div class="col-1-3">
+        <ul>
+          <li>
+            <h4>HR</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>Buildings &amp; facilities</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>Financial management</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>Security</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>IT services</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-1-3">
+        <ul>
+          <li>
+            <h4>Fire, health &amp; safety</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>Commercial &amp; contract management</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>Communications</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+          <li>
+            <h4>Knowledge &amp; information</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam nibh.</p>
+          </li>
+        </ul>
+        <div class="more">
+          <a href="">See all</a>
+        </div>
+      </div>
+    </div>
+  </div>
+   <div class="content-row">
+  </div>
+  <div class="content-row">
+  </div>
+</div>
