2fcd05046f3700537fcea515cb7b2d4d104af6c2
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-16 15:40:35 +0100

    reorganised the scss files

--- application/views/v2/layouts/default.php
+++ application/views/v2/layouts/default.php
@@ -6,9 +6,6 @@
 <body>
   <div class="main-container">
     <div class="main-container-inner">
-      <?php $this->load->view('v'.version().'/modules/header'); ?>
-      <?php $this->load->view('v'.version().'/content/'.$page); ?>
-      <?php $this->load->view('v'.version().'/modules/footer'); ?>
     </div>
   </div>
 </body>
