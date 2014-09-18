387498fe74a112277564cdf3ca552cf1343032a6
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-18 10:56:07 +0100

    made header/subheader background span across viewport; styled placeholder images (pure CSS)

--- application/views/v1/modules/header.php
+++ application/views/v1/modules/header.php
@@ -1,32 +1,34 @@
-<header class="page-header clearfix grid grid-pad">
-  <div class="logo col-1-2">
-    <img src="assets/images/logo.png" />
-    <span>Ministry of Justice</span>
-  </div>
+<header class="page-header">
+  <div class="grid grid-pad">
+    <div class="logo col-1-2">
+      <img src="assets/images/logo.png" />
+      <span>Ministry of Justice</span>
+    </div>
 
-  <nav class="top-nav col-1-2">
-    <ul>
-      <li>
-        <a href="">Home</a>
-      </li>
-      <li>
-        <a href="">News &amp; Features</a>
-      </li>
-      <li>
-        <a href="">Guidance &amp; Support</a>
-      </li>
-      <li>
-        <a href="">Other Intranet links</a>
-      </li>
-      <li>
-        <a href="">Ministers &amp; Parliament</a>
-      </li>
-      <li>
-        <a href="">Organisation</a>
-      </li>
-      <li>
-        <a href="">The MoJ Story</a>
-      </li>
-    </ul>
-  </nav>
+    <nav class="top-nav col-1-2">
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">News &amp; Features</a>
+        </li>
+        <li>
+          <a href="">Guidance &amp; Support</a>
+        </li>
+        <li>
+          <a href="">Other Intranet links</a>
+        </li>
+        <li>
+          <a href="">Ministers &amp; Parliament</a>
+        </li>
+        <li>
+          <a href="">Organisation</a>
+        </li>
+        <li>
+          <a href="">The MoJ Story</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </header>
--- application/views/v2/content/guidance_and_support/sub_header.php
+++ application/views/v2/content/guidance_and_support/sub_header.php
@@ -1,29 +1,31 @@
-<div class="sub-header grid grid-pad">
-  <nav class="tabs col-1-2">
-    <ul>
-      <li>
-        <a href="news_and_features">News &amp; events</a>
-      </li>
-      <li class="selected">
-        <a href="guidance_and_support">Guidance &amp; support</a>
-      </li>
-      <li>
-        <a href="about">About MoJ</a>
-      </li>
-    </ul>
-  </nav>
+<div class="sub-header">
+  <div class="grid grid-pad">
+    <nav class="tabs col-1-2">
+      <ul>
+        <li>
+          <a href="news_and_features">News &amp; events</a>
+        </li>
+        <li class="selected">
+          <a href="guidance_and_support">Guidance &amp; support</a>
+        </li>
+        <li>
+          <a href="about">About MoJ</a>
+        </li>
+      </ul>
+    </nav>
 
-  <nav class="top-nav col-1-2 push-1-2">
-    <ul>
-      <li>
-        <a href="">A-Z</a>
-      </li>
-      <li>
-        <a href="">Forms &amp template</a>
-      </li>
-      <li>
-        <a href="">Other intranet links</a>
-      </li>
-    </ul>
-  </nav>
+    <nav class="top-nav col-1-2">
+      <ul>
+        <li>
+          <a href="">A-Z</a>
+        </li>
+        <li>
+          <a href="">Forms &amp template</a>
+        </li>
+        <li>
+          <a href="">Other intranet links</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </div>
