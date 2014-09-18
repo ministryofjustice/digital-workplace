2a026948b591e71249a7cea2a7f43a672fdfab69
Jonathan Conway <jonathan@jaikoo.com>
2014-09-18 12:48:35 +0100

    added compass config

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'assets/stylesheets'
+sass_dir = 'assets/sass'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/js'
+relative_assets = true
+line_comments = true
