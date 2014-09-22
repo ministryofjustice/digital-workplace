4d8e669168a45045051a9a84488832827fdfe59a
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-22 17:19:09 +0100

    fixed an issue with the mobile menu being hidden on other breakpoints when it's been interacted with on mobile breakpoint

--- assets/js/app/v2/index.js
+++ assets/js/app/v2/index.js
@@ -5,6 +5,12 @@ $(function(){
     $('.mobile-menu').click(function(){
       $('.sub-header .tabs').slideToggle(100);
     });
+
+    $(window).bind('breakpointChange', function(e, breakpoint){
+      if(breakpoint!=='mobile'){
+        $('.sub-header .tabs').get(0).style.display = '';
+      }
+    });
   };
 
   App.headerMenu();
