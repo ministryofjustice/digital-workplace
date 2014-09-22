b826b52e4decc16c953c22c7f75ad6d309d22704
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-22 17:17:53 +0100

    implemented breakpointChange event triggering

--- application/views/shared/body_bottom.php
+++ application/views/shared/body_bottom.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="assets/js/vendor/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/app/shared/app.js"></script>
+<script type="text/javascript" src="assets/js/app/shared/breakpoints.js"></script>
 <script type="text/javascript" src="assets/js/app/v<?=version()?>/index.js"></script>
