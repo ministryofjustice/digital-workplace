076b7482d57015e4d661bb7205f8fb7b4cf7d629
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-23 17:08:09 +0100

    updated the footer to be mobile-friendly

--- application/views/v2/modules/footer.php
+++ application/views/v2/modules/footer.php
@@ -1,69 +1,67 @@
 <footer>
   <div class="grid grid-pad">
-    <div class="col-1-3">
+    <div class="information col-1-3">
       <h5>Your digital workplace</h5>
+      <ul>
+        <li>
+          <a href="">Corporate blog</a>
+        </li>
+        <li>
+          <a href="">GOV.UK</a>
+        </li>
+        <li>
+          <a href="">RSS feeds</a>
+        </li>
+        <li>
+          <a href="">Intranet feedback form</a>
+        </li>
+        <li>
+          <a href="">Submit a news story</a>
+        </li>
+        <li>
+          <a href="">Help</a>
+        </li>
+        <li>
+          <a href="">Cookies</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
     </div>
-    <div class="col-1-3">
-      <h5>Other department intranet</h5>
-    </div>
-  </div>
-  <div class="grid grid-pad">
-    <ul class="information col-1-3">
-      <li>
-        <a href="">Corporate blog</a>
-      </li>
-      <li>
-        <a href="">GOV.UK</a>
-      </li>
-      <li>
-        <a href="">RSS feeds</a>
-      </li>
-      <li>
-        <a href="">Intranet feedback form</a>
-      </li>
-      <li>
-        <a href="">Submit a news story</a>
-      </li>
-      <li>
-        <a href="">Help</a>
-      </li>
-      <li>
-        <a href="">Cookies</a>
-      </li>
-      <li>
-        <a href="">Contact</a>
-      </li>
-    </ul>
 
-    <ul class="other col-1-3">
-      <li>
-        <a href="">HM Courts & Tribunals Service</a>
-      </li>
-      <li>
-        <a href="">JAC</a>
-      </li>
-      <li>
-        <a href="">Judicial Office</a>
-      </li>
-      <li>
-        <a href="">Law Commission</a>
-      </li>
-      <li>
-        <a href="">OPG</a>
-      </li>
-      <li>
-        <a href="">OSPT</a>
-      </li>
-      <li>
-        <a href="">Probation</a>
-      </li>
-      <li>
-        <a href="">Reset</a>
-      </li>
-      <li>
-        <a href="">Other links & sites</a>
-      </li>
-    </ul>
+    <div class="other col-1-3">
+      <h5>Other department intranet</h5>
+      <ul >
+        <li>
+          <a href="">HM Courts & Tribunals Service</a>
+        </li>
+        <li>
+          <a href="">JAC</a>
+        </li>
+        <li>
+          <a href="">Judicial Office</a>
+        </li>
+        <li>
+          <a href="">Law Commission</a>
+        </li>
+        <li>
+          <a href="">OPG</a>
+        </li>
+        <li>
+          <a href="">OSPT</a>
+        </li>
+        <li>
+          <a href="">Probation</a>
+        </li>
+        <li>
+          <a href="">Reset</a>
+        </li>
+        <li>
+          <a href="">Other links & sites</a>
+        </li>
+      </ul>
+    </div>
   </div>
   <div class="grid grid-pad">
     <div class="col-1-1">
