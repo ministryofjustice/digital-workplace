25f62f30f3f74a6bc85aad51c6f5841a0604afdf
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-09-23 13:29:15 +0100

    added new breakpoint MQs

--- application/views/shared/head.php
+++ application/views/shared/head.php
@@ -1,3 +1,4 @@
 <title>Page Title</title>
 <base href="<?=base_url()?>" />
 <link rel="stylesheet" type="text/css" href="assets/stylesheets/v<?=version()?>/index.css" />
+<meta name="viewport" content="width=device-width, user-scalable=no">
